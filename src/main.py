4479ad6011fc1dc5d306dfcf846955af4c9d88b7
Hartsaxena <64151017+Hartsaxena@users.noreply.github.com>
2022-06-14 18:29:08 -0500

    First optimization
    Added comments to code, Added timing system, and basically attempted to optimize everything.

--- src/main.py
+++ src/main.py
@@ -1,44 +1,42 @@
 import random
-import os
-from pathlib import Path
-os.chdir(Path(__file__).parent.absolute())
+import time
 
 
 def simulation(switch):
+    '''
+    Runs a single iteration of the Monty Hall simulation.
+    the argument 'switch' is a boolean value that represents whether to switch doors after the host reveals a goat.
+    '''
     doors = [0, 0, 0] # 0 = donkey, 1 = lamborghini
-    doors[random.randint(0,2)] = 1
-    choice = random.randint(0,2)
-    if random.randint(0, 1) == 0:
-        host_gen = range(0, 3, 1)
+    doors[random.randint(0,2)] = 1 # Make one of the doors the lamborghini
+    choice = random.randint(0,2) # The player walks next to a random door.
+
+    if switch: # Simulate the player either switching or not switching doors
+        # Find which door the host will reveal, and use it to find the switching position
+        for door_index in range(len(doors)):
+            if door_index != choice and doors[door_index] == 0: # Rules for which door the host reveals: not the player's choice and not the prize.
+                return doors[3 - choice - door_index] # Find which door the player would switch to if they switch
     else:
-        host_gen = range(2, -1, -1)
-    for door_index in host_gen:
-        if door_index != choice and doors[door_index] == 0:
-            host_choice = door_index
-            break
-    if switch:
-        doors = [prize for door, prize in enumerate(doors) if door != choice and door != host_choice]
-        if doors[0]:
-            return choice, switch, True
-        else:
-            return choice, switch, False
-    else:
-        if doors[choice]:
-            return choice, switch, True
-        else:
-            return choice, switch, False
+        return doors[choice]
     
 
 def main():
-    data = [0, 0]
-    sim_count = 1_000
-    for switch in range(0, 2):
-        print(f"Running {sim_count} simulations for switch={switch}...")
-        for _ in range(0, sim_count):
-            simulation_results = simulation(bool(switch))
-            data[switch] += int(simulation_results[2])
+    win_losses = [[0, 0], [0, 0]] # Amount of wins and losses for not switching doors or switching doors, respectively.
+    current_time = time.perf_counter()
+    time_limit = 1 # Amount of time the simulation runs
+    switch = True
+    while time.perf_counter() - current_time < time_limit:
+        switch = not switch # Alternate between switching and not switching doors to make simulation more accurate.
+        simulation_result = simulation(switch)
+        win_losses[int(switch)][1 - simulation_result] += 1 # Record simulation result
+    
+    total_sims = sum(map(sum, win_losses))
+    print(f"After {time_limit} seconds, the program ran {total_sims} simulations.\nThe player won {win_losses[1][0]} times when they switched doors.\nThe player won {win_losses[0][0]} times when they didn't switch doors.")
+    print("\nAccording to this iteration of simulations:")
+    print(f"The player wins {win_losses[1][0] / sum(win_losses[1]) * 100}% of the time when they switch.")
+    print(f"The player wins {win_losses[0][0] / sum(win_losses[0]) * 100}% of the time when they don't switch.")
 
-    print(f"Ran {sim_count} simulations. Switching won a car {data[1]} times. Not switching won a car {data[0]} times.")
+    
 
 
 if __name__ == '__main__':
