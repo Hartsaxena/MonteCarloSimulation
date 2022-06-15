c981a19c85a54187a6a6b293c074f4d09a439caa
Meepatron <57198749+Meepatron300@users.noreply.github.com>
2022-06-15 07:59:47 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+import random
+from time import perf_counter
+
+stay_wins = 0
+switch_wins = 0
+time_limit = 60
+
+while perf_counter() < time_limit:
+    if random.randint(1, 3) == 1:
+        stay_wins += 1
+    else:
+        switch_wins += 1
+
+print(f"After {time_limit} seconds, the program ran {stay_wins + switch_wins}.")
+print()
+print(f"The player wins {switch_wins / (stay_wins + switch_wins) * 100}% of the time when they switch.")
+print(f"The player wins {stay_wins / (stay_wins + switch_wins) * 100}% of the time when they don't switch.")
